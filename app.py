8f506841602ad13014f8e11ace674259f6ef06aa
Bishnuhaldar <109227807+Bishnuhaldar@users.noreply.github.com>
2024-11-28 13:02:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 # Configure the OpenAI API
-key = "AIzaSyB2Ap-o973pkpyvPaKiktbZwd4LX1FxU2c"
+key = "AIzaSyBuuk83zH6aEYiO4YKVLwgABvuDf2GHR70"
 genai.configure(api_key=key)
 generation_config = {
     "temperature": 1,
