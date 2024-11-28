9fd6d89fbadd2eb149c1df93e47cb583d5c3a5cc
Lagnajeet-CG <121150082+Lagnajeet-CG@users.noreply.github.com>
2024-11-28 13:15:03 +0530

    Update app.py
    Changing the logo - line 113

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ st.markdown("""
 st.markdown('<h1>Strategic AI Guidance Engine🤖</h1>', unsafe_allow_html=True)
 
 # # Path to your logo
-logo_path2 = "SAGE.png"
+logo_path2 = "SAGE1.png"
 
 # Display the logo in the sidebar
 st.sidebar.image(logo_path2, use_column_width=True)
